2ab589a1b12ea2c7e2c7986e2c2c641a44cc4beb
Linduff,Caleb <caleb_linduff@vanguard.com>
2024-08-13 17:49:45 +0000

    added poster example and pagination

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 # popular_movies = tmdb.movie(1022789)
 # print(popular_movies)
 
-@app.route("/")
-def main():
-    popular_movies = tmdb.popular_movies()
-    return render_template("home.html", movies = popular_movies)
+# @app.route("/")
+# def main():
+#     popular_movies = tmdb.popular_movies()
+#     return render_template("home.html", movies = popular_movies)
 
-@app.route("/search")
+@app.route("/")
 def search():
     searchResults = json.load(open('testFiles/searchResults_star_wars.json'))
     return render_template("search.html", searchResults=searchResults)
@@ -21,9 +21,9 @@ def search():
 # def search(query):
 #     return render_template("search.html", results=jsonify(tmdb.search_movies(query)))
 
-@app.route("/movie/<movie_id>")
-def movie(movie_id):
-    return render_template("movie.html", movie=tmdb.movie(movie_id))
+# @app.route("/movie/<movie_id>")
+# def movie(movie_id):
+#     return render_template("movie.html", movie=tmdb.movie(movie_id))
 
 @app.route('/healthcheck')
 def health():
