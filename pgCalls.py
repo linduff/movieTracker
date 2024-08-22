72358ad013b31fb80e1a17022ebd6335078c370a
Linduff,Caleb <caleb_linduff@vanguard.com>
2024-08-22 14:36:59 +0000

    Added table create strings

--- pgCalls.py
+++ pgCalls.py
@@ -5,20 +5,90 @@ from pgConfig import load_config
 
 print("starting pgCalls")
 
+create_movie_details_cache_table = """
+CREATE TABLE IF NOT EXISTS movie_details_cache (
+	movie_id INT PRIMARY KEY,
+	updated_at TIMESTAMP NOT NULL,
+	backdrop_path TEXT,
+	belongs_to_collection INT[],
+	budget INT
+	genres INT[],
+	homepage TEXT,
+	imdb_id TEXT,
+	origin_country TEXT[],
+	original_language TEXT,
+	original_title TEXT,
+	overview TEXT,
+	popularity FLOAT4,
+	poster_path TEXT,
+	production_companies INT[],
+	production_countries TEXT[],
+	release_date TEXT,
+	revenue INT,
+	runtime INT,
+	spoken_languages TEXT[],
+	status TEXT,
+	tagline TEXT,
+	title TEXT
+)
+"""
+
+create_movie_collections_table = """
+CREATE TABLE IF NOT EXISTS movie_collections (
+	id INT PRIMARY KEY,
+	name TEXT,
+	poster_path TEXT,
+	backdrop_path TEXT
+)
+"""
+
+create_movie_genres_table = """
+CREATE TABLE IF NOT EXISTS movie_genres (
+	id INT PRIMARY KEY,
+	name TEXT,
+)
+"""
+
+create_movie_origin_countries_table = """
+CREATE TABLE IF NOT EXISTS movie_origin_countries (
+	id SERIAL PRIMARY KEY,
+	country_name TEXT,
+)
+"""
+
+create_movie_production_companies_table = """
+CREATE TABLE IF NOT EXISTS movie_production_companies (
+	id INT PRIMARY KEY,
+	logo_path TEXT,
+	name TEXT,
+	origin_country TEXT
+)
+"""
+
+create_movie_production_countries_table = """
+CREATE TABLE IF NOT EXISTS movie_production_countries (
+	id SERIAL PRIMARY KEY,
+	iso_3166_1 TEXT,
+	name TEXT
+)
+"""
+
+create_movie_movie_spoken_languages_table = """
+CREATE TABLE IF NOT EXISTS movie_spoken_languages (
+	id SERIAL PRIMARY KEY,
+	english_name TEXT,
+	iso_639_1 TEXT,
+	name TEXT
+)
+"""
+
 def create_movie_details_cache_table(table_name):
-    command = """
-            CREATE TABLE IF NOT EXISTS movie_details_cache (
-                movie_id INT PRIMARY KEY,
-                updated_at TIMESTAMP NOT NULL,
-                json_data JSONB NOT NULL
-            )
-            """
     config = load_config()
 
     try:
         with psycopg2.connect(**config) as conn:
             with conn.cursor() as cur:
-                  cur.execute(command)
+                  cur.execute(create_movie_details_cache_table)
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
